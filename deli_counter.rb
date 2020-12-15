a6c75b0ba43564e95de082fd952915d288ac9805
ritchey0713 <ritchey0713@gmail.com>
2020-12-15 18:51:39 -0500

    commit tree 11845be322be2de5255542c479035e55f8523daf

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,36 @@
-# Write your code here.
+# "The line is currently: 1. Logan 2. Avi 3. Spencer"
+
+require 'pry'
+
+def line(deli) 
+  if deli.empty? 
+    puts "The line is currently empty."
+  else 
+    # deli.push(deli) (why it didnt work)
+    # deli = ["Logan", "Avi", "Spencer"]
+    queue = "The line is currently:"
+    deli.each.with_index(1) do |name, i|
+      queue << " #{i}. #{name}"
+    end 
+    puts queue
+  end 
+end 
+# got: ("The line is currently: Logan" "The line is currently: Avi")
+
+def take_a_number(deli, person)
+  if deli.empty? 
+    deli.push(person)
+    puts "Welcome, #{person}. You are number 1 in line."
+  else 
+    deli.push(person)
+    puts "Welcome, #{person}. You are number #{deli.length} in line."
+  end 
+end 
+
+def now_serving(deli)
+  if deli.empty? 
+    puts "There is nobody waiting to be served!"
+  else 
+    puts "Currently serving #{deli.shift}."
+  end 
+end 
